6fc7a0a1476e8e99456276d677ea592a0cdacd42
Xuemao Zhang <43715985+xuemaozhang@users.noreply.github.com>
2021-11-19 10:47:43 -0500

    Update app_server.R

--- R/app_server.R
+++ R/app_server.R
@@ -4,6 +4,7 @@
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
+values = reactiveValues(data_imported=NULL)
 app_server <- function( input, output, session ) {
   # Your application server logic 
   mod_module1_data_server("module1_data_ui_1")
